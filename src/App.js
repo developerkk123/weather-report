8b01719f44e4d2a3e6daf4987ac89e7c84dc25c1
kundan <kundan505020@gmail.com>
2020-06-02 16:04:11 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import './App.css';
-
-import Bitgraph from './Dashboard/Graph/BitdevGraph/Bitgraph';
 import Scrollanimation from './Dashboard/ScrollAnimation/Scrollanimation';
 import Animationonscroll from './Dashboard/ScrollAnimation/Scrollanimation';
 
 function App() {
   return (
     <div className="App">
-       {/* <Bitgraph /> */}
       <Animationonscroll />
-     
     </div>
   );
 }
--- src/Dashboard/ScrollAnimation/Scrollanimation.js
+++ src/Dashboard/ScrollAnimation/Scrollanimation.js
@@ -1,44 +1,176 @@
 import React, { Component } from 'react';
 import "animate.css/animate.min.css";
 import ScrollAnimation from 'react-animate-on-scroll';
-import './style.scss'
+import { Card, Image, Icon } from 'semantic-ui-react';
+import image from '../../asset/image/techskill.jpg';
+import './style.scss';
+
 class Animationonscroll extends Component {
     render() {
         return (
             <div>
                 <h1>Scroll Animation Demo</h1>
                 <ScrollAnimation animateIn='fadeIn' animateOut='fadeOut' className="animation-scroll">
-                    <h1>React Animate On Scroll</h1>
-                    <h2> Using:</h2>
+                    <Card>
+                        <Image src={image} wrapped ui={false} />
+                        <Card.Content>
+                            <Card.Header>@tech.skill_s</Card.Header>
+                            <Card.Description>
+                                Web developer
+                                🔥Full stack Engineer 🔥
+                                📚 HTML/CSS/JavaScript/MySQL/React/Java
+                                📑 Code snippets🔥
+                                📩 DM for coding &placement related quer
+                            </Card.Description>
+                        </Card.Content>
+                        <Card.Content extra>
+                            <a><Icon name='user' />585 Followers</a>
+                        </Card.Content>
+                    </Card>
                 </ScrollAnimation>
                 <ScrollAnimation animateIn='bounceInRight' animateOut='bounceOutLeft' className="animation-scroll">
-                    <h2><a href='https://daneden.github.io/animate.css/'>Animate.css</a></h2>
-                </ScrollAnimation>
-                <ScrollAnimation animateIn='fadeIn'
-                    animateOut='fadeOut'>
-                    <h1>React Animate On Scroll</h1>
-                    <h2>Using:</h2>
+                <Card>
+                        <Image src={image} wrapped ui={false} />
+                        <Card.Content>
+                            <Card.Header>@tech.skill_s</Card.Header>
+                            <Card.Description>
+                                Web developer
+                                🔥Full stack Engineer 🔥
+                                📚 HTML/CSS/JavaScript/MySQL/React/Java
+                                📑 Code snippets🔥
+                                📩 DM for coding &placement related quer
+                            </Card.Description>
+                        </Card.Content>
+                        <Card.Content extra>
+                            <a><Icon name='user' />585 Followers</a>
+                        </Card.Content>
+                    </Card>
                 </ScrollAnimation>
                 <ScrollAnimation animateIn='bounceInRight' animateOut='bounceOutLeft' className="animation-scroll">
-                    <h2><a href='https://daneden.github.io/animate.css/'>Animate.css</a></h2>
+                <Card>
+                        <Image src={image} wrapped ui={false} />
+                        <Card.Content>
+                            <Card.Header>@tech.skill_s</Card.Header>
+                            <Card.Description>
+                                Web developer
+                                🔥Full stack Engineer 🔥
+                                📚 HTML/CSS/JavaScript/MySQL/React/Java
+                                📑 Code snippets🔥
+                                📩 DM for coding &placement related quer
+                            </Card.Description>
+                        </Card.Content>
+                        <Card.Content extra>
+                            <a><Icon name='user' />585 Followers</a>
+                        </Card.Content>
+                    </Card>
                 </ScrollAnimation>
                 <ScrollAnimation animateIn='fadeIn' className="animation-scroll">
-                    <h1>animateIn</h1>
+                <Card>
+                        <Image src={image} wrapped ui={false} />
+                        <Card.Content>
+                            <Card.Header>@tech.skill_s</Card.Header>
+                            <Card.Description>
+                                Web developer
+                                🔥Full stack Engineer 🔥
+                                📚 HTML/CSS/JavaScript/MySQL/React/Java
+                                📑 Code snippets🔥
+                                📩 DM for coding &placement related quer
+                            </Card.Description>
+                        </Card.Content>
+                        <Card.Content extra>
+                            <a><Icon name='user' />585 Followers</a>
+                        </Card.Content>
+                    </Card>
                 </ScrollAnimation>
                 <ScrollAnimation animateIn='flipInY' animateOut='flipOutY' className="animation-scroll">
-                    <h1>animateOut</h1>
+                <Card>
+                        <Image src={image} wrapped ui={false} />
+                        <Card.Content>
+                            <Card.Header>@tech.skill_s</Card.Header>
+                            <Card.Description>
+                                Web developer
+                                🔥Full stack Engineer 🔥
+                                📚 HTML/CSS/JavaScript/MySQL/React/Java
+                                📑 Code snippets🔥
+                                📩 DM for coding &placement related quer
+                            </Card.Description>
+                        </Card.Content>
+                        <Card.Content extra>
+                            <a><Icon name='user' />585 Followers</a>
+                        </Card.Content>
+                    </Card>
                 </ScrollAnimation>
                 <ScrollAnimation animateIn='wobble' initiallyVisible={true} className="animation-scroll">
-                    <h1>initiallyVisible</h1>
+                <Card>
+                        <Image src={image} wrapped ui={false} />
+                        <Card.Content>
+                            <Card.Header>@tech.skill_s</Card.Header>
+                            <Card.Description>
+                                Web developer
+                                🔥Full stack Engineer 🔥
+                                📚 HTML/CSS/JavaScript/MySQL/React/Java
+                                📑 Code snippets🔥
+                                📩 DM for coding &placement related quer
+                            </Card.Description>
+                        </Card.Content>
+                        <Card.Content extra>
+                            <a><Icon name='user' />585 Followers</a>
+                        </Card.Content>
+                    </Card>
                 </ScrollAnimation>
                 <ScrollAnimation duration={5} animateIn='hinge' initiallyVisible={true} className="animation-scroll">
-                    <h1>duration</h1>
+                <Card>
+                        <Image src={image} wrapped ui={false} />
+                        <Card.Content>
+                            <Card.Header>@tech.skill_s</Card.Header>
+                            <Card.Description>
+                                Web developer
+                                🔥Full stack Engineer 🔥
+                                📚 HTML/CSS/JavaScript/MySQL/React/Java
+                                📑 Code snippets🔥
+                                📩 DM for coding &placement related quer
+                            </Card.Description>
+                        </Card.Content>
+                        <Card.Content extra>
+                            <a><Icon name='user' />585 Followers</a>
+                        </Card.Content>
+                    </Card>
                 </ScrollAnimation>
                 <ScrollAnimation delay={4000} animateIn='tada' initiallyVisible={true} className="animation-scroll">
-                    <h1>delay</h1>
+                <Card>
+                        <Image src={image} wrapped ui={false} />
+                        <Card.Content>
+                            <Card.Header>@tech.skill_s</Card.Header>
+                            <Card.Description>
+                                Web developer
+                                🔥Full stack Engineer 🔥
+                                📚 HTML/CSS/JavaScript/MySQL/React/Java
+                                📑 Code snippets🔥
+                                📩 DM for coding &placement related quer
+                            </Card.Description>
+                        </Card.Content>
+                        <Card.Content extra>
+                            <a><Icon name='user' />585 Followers</a>
+                        </Card.Content>
+                    </Card>
                 </ScrollAnimation>
                 <ScrollAnimation animateIn='bounce' initiallyVisible={true} animateOnce={true} className="animation-scroll">
-                    <h1>animateOnce</h1>
+                <Card>
+                        <Image src={image} wrapped ui={false} />
+                        <Card.Content>
+                            <Card.Header>@tech.skill_s</Card.Header>
+                            <Card.Description>
+                                Web developer
+                                🔥Full stack Engineer 🔥
+                                📚 HTML/CSS/JavaScript/MySQL/React/Java
+                                📑 Code snippets🔥
+                                📩 DM for coding &placement related quer
+                            </Card.Description>
+                        </Card.Content>
+                        <Card.Content extra>
+                            <a><Icon name='user' />585 Followers</a>
+                        </Card.Content>
+                    </Card>
                 </ScrollAnimation>
                 <ScrollAnimation animateIn='flipInX'
                     afterAnimatedIn={function afterAnimatedIn(v) {
@@ -47,7 +179,22 @@ class Animationonscroll extends Component {
                         t += 'v.inViewport: ' + v.inViewport;
                         alert(t);
                     }}>
-                    <h1>afterAnimatedIn</h1>
+                    <Card>
+                        <Image src={image} wrapped ui={false} />
+                        <Card.Content>
+                            <Card.Header>@tech.skill_s</Card.Header>
+                            <Card.Description>
+                                Web developer
+                                🔥Full stack Engineer 🔥
+                                📚 HTML/CSS/JavaScript/MySQL/React/Java
+                                📑 Code snippets🔥
+                                📩 DM for coding &placement related quer
+                            </Card.Description>
+                        </Card.Content>
+                        <Card.Content extra>
+                            <a><Icon name='user' />585 Followers</a>
+                        </Card.Content>
+                    </Card>
                 </ScrollAnimation>
             </div>
         );
