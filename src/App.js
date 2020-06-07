d9b5624c3eb12efd0c8e331fafe9429ba0296722
kundan <kundan505020@gmail.com>
2020-06-07 12:31:55 +0530

    checkbox

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import './App.css';
-// import Animationonscroll from './Dashboard/Examples/ScrollAnimation/Scrollanimation';
-import Numbercount from './Dashboard/Examples/NumberCountup/Numbercount';
-
+import Homepage from './Dashboard/HomePage'
 function App() {
   return (
     <div className="App">
-      <Numbercount />
-      {/* <Animationonscroll /> */}
+       <Homepage />
     </div>
   );
 }
---
+++ src/Dashboard/HomePage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Homepage extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Multiple Checkbox Demo</h1>
+            </div>
+        )
+    }
+}
+export default Homepage;
